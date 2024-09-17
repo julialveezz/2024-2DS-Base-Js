4ce106e65fdbd3690379d3662fed0c94f6937f88
Julia Emanuelle Alves <160873501+julialveezz@users.noreply.github.com>
2024-09-17 12:10:43 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,17 @@
+// Tipo  Number
+// Var era utilizado para todas as variáveis
+var numero01 = 3
+// let é uma atualização do var e é possível e variação
+let numero02 = 10
+// const é uma maneira de declarar uma variável que irá permanecer constante
+const numero03 = 7
+
+const soma = numero01 + numero03
+const subtracao = numero02 - numero01
+const multiplicacao = numero03 * numero02
+const divisao = numero02 / numero01
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
